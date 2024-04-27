a85c98a2bd959b64476f46dc2791915d063c843f
LucasPereira01 <lucaspdocarmo@gmail.com>
2024-04-27 15:33:22 -0300

    chore:   testing  prettier and eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <div className="bg-red-700 py-5">Hello TailWind</div>;
+  return (
+    <div className="bg-red-700 py-5">
+      <h1>Hello</h1>
+      Hello TailWind
+    </div>
+  );
 };
 
 export default Home;

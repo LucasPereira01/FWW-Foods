be6e6a535c0ad1af881a4d19efb6aeadbaf4f396
LucasPereira01 <lucaspdocarmo@gmail.com>
2024-04-30 10:07:15 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
+import Header from "./_components/header";
+
 const Home = () => {
-  return (
-    <div className="bg-red-700 py-5">
-      <h1>Hello</h1>
-      Hello TailWind
-    </div>
-  );
+  return <Header />;
 };
 
 export default Home;
